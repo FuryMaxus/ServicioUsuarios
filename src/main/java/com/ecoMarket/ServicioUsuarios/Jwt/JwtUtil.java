cee5d83b0297c6a9aea7b1c8a4f6a7d53114610a
Jose Valenzuela <jivalenzuela2010@hotmail.com>
2025-11-22 16:03:27 -0300

    ahora el token manda los roles del usuario, la clave ahora esta separada del codigo

--- src/main/java/com/ecoMarket/ServicioUsuarios/Jwt/JwtUtil.java
+++ src/main/java/com/ecoMarket/ServicioUsuarios/Jwt/JwtUtil.java
@@ -3,20 +3,31 @@ package com.ecoMarket.ServicioUsuarios.Jwt;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtUtil {
-    private final String SECRET_KEY = "clave_secreta_increiblemente_segura_:)";
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
 
     public String generateToken(UserDetails userDetails) {
+
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+
         return Jwts.builder()
                 .setSubject(userDetails.getUsername())
+                .claim("roles", roles)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 horas
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
                 .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()), SignatureAlgorithm.HS256)
                 .compact();
     }
